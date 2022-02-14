05bc97e5f6b3d9af76a218e0427f8391e3c8c99b
Artur Komaristych <artur@s0pex.me>
2022-02-14 15:05:47 +0100

    Removes tanken and adds Lidl.de

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,33 @@
-import { Telegraf, Context } from "telegraf";
-import { Message } from "typegram";
-import chron from "node-cron";
-import { GasType, SortType, tankKoenigApi } from "./tanken-api";
+import axios from 'axios';
+import cron from 'node-cron';
+import parse from 'node-html-parser';
+import { Gotify } from 'gotify';
 
-let e5Threshold = 1.45;
-const bot = new Telegraf(process.env.BOT_TOKEN as any);
-
-const getStations = () => {
-  return tankKoenigApi.getStationsByLocation(
-    50.963604,
-    7.142388,
-    5,
-    SortType.Price,
-    GasType.E5
-  );
-};
-
-chron.schedule("*/15 * * * *", async () => {
-  getStations().then((data) => {
-    data.stations?.forEach((station) => {
-      const price = station.e5 ?? station.price;
-      if (price && price < e5Threshold) {
-        bot.telegram.sendMessage(
-          -534885489,
-          `Es wurde eine Tankstelle mit einem Preis von ${station.e5}€ gefunden.\n
-                Name: ${station.brand}-${station.name}
-                Adresse: https://maps.google.com/?ll=${station.lat},${station.lng}`
-        );
-      }
-    });
-  });
+const push_message_client = new Gotify({
+  server: 'https://gotify.s0pex.me',
 });
 
-bot.command("chatid", (ctx: Context) =>
-  ctx.reply(`Dieser Chat hat die Chat-Id: ${ctx.chat?.id}`)
-);
-bot.command("setPriceE5", (ctx: Context) => {
-  const msg = ctx.message as Message.TextMessage;
-  const args = msg.text.split(" ");
-  try {
-    if (args.length < 2)
-      throw "Incorrect use of this command!\nUsage: /setPriceE5 {price}\nExample: /setPriceE5 1.45";
-    const price = Number.parseFloat(args[1]);
-    if (isNaN(price)) throw "The price has to be a number!";
-    e5Threshold = Number.parseFloat(args[1]);
-    ctx.reply(`Set price to ${e5Threshold}€`);
-  } catch (error) {
-    ctx.reply(error);
+cron.schedule('*/15 * * * *', async () => {
+  const url =
+    'https://www.lidl.de/p/kessebohmer-schreibtisch-elektrisch-hohenverstellbar/p100313116029';
+  const res = await axios.get(url);
+
+  const root = parse(res.data);
+  const price_text = root.querySelector('.m-price__price')?.text;
+  if (price_text != null) {
+    const price = parseFloat(price_text);
+    console.log(`Checked Lidl.de with price of ${price}`);
+    if (price < 410) {
+      push_message_client.send({
+        app: 'Ag8fWlPelQImiWl',
+        title: 'Lidl.de',
+        message: `Der Kesseböhmer Schreibtisch ist wieder lieferbar.\nLink: ${url}`,
+        priority: 5,
+      });
+    }
   }
 });
-bot.command("current", (ctx: Context) => {
-  getStations().then((data) => {
-    var text = data.stations
-      ?.map(
-        (station) =>
-          `Name: ${station.brand} ${station.name}\nPreis: ${
-            station.e5 || station.price
-          }\nAdresse: https://maps.google.com/?ll=${station.lat},${station.lng}`
-      )
-      .join("\n\n");
-    ctx.reply(text);
-  });
-});
-bot.launch();
 
 // Enable graceful stop
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+//process.once("SIGINT", () => chron("SIGINT"));
+//process.once("SIGTERM", () => bot.stop("SIGTERM"));
